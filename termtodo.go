2ad0bc66b65859b511b33b0a39235786fd410ea1
shaojiale <shaojiale@bytedance.com>
2021-10-14 11:15:51 +0800

    chore: rm useless code

--- termtodo.go
+++ termtodo.go
@@ -4,8 +4,6 @@ import (
 	"github.com/SCU-SJL/termtodo/core"
 	"github.com/SCU-SJL/termtodo/screen"
 	"github.com/SCU-SJL/termtodo/todolist"
-	"os"
-	"time"
 )
 
 func main() {
@@ -14,21 +12,7 @@ func main() {
 	defer screen.DefaultScreen.Fini()
 	defer todolist.Save()
 
-	//test()
-
 	core.Init(screen.DefaultScreen.SetCursorLine(1))
 	core.Dispatcher.Dispatch()
 
-}
-
-func test() {
-	s := screen.DefaultScreen
-	x, y := screen.DefaultScreen.Size()
-	s.SetContent(x/2, y/2, "1. test")
-	s.SetContent(x/2, y/2+1, "2. test")
-	for {
-		s.Show()
-		time.Sleep(2 * time.Second)
-		os.Exit(0)
-	}
 }
